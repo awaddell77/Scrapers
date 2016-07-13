86a26baa6f3d9c52dbd9c8193ca95a5a870f484d
awaddell77 <waddell.andrew@gmail.com>
2016-07-13 12:19:27 -0700

    Added CSV writing functionality

--- soup_mlp.py
+++ soup_mlp.py
@@ -16,6 +16,7 @@ def splitter(x):
 	table = site.find('div', {'id':'mw-content-text'}).table.table
 	rows = table.find_all('tr')
 	crit = ['Name', 'Card Type', 'Color', 'Traits', 'Game Text', 'Power', 'Image Link', 'Image']
+	bad = ('\n','\u2022')
 	d = {}
 	d['Name'] = site.find('div',{'class':'header-container'}).h1.text
 	d["Image Link"] = S_format(str(site.find('div', {'id':'mw-content-text'}).p.img)).linkf('src=')
@@ -24,9 +25,9 @@ def splitter(x):
 		cells = rows[i].find_all('td')
 		if len(cells) == 2:
 			if d.get(cells[0].text, 'None found') != 'None found':
-				d[cells[0].text] = d[cells[0].text] + ' // ' + cleaner(cells[1].text, '\n')
+				d[cells[0].text] = d[cells[0].text] + ' // ' + cleaner(cells[1].text, bad)
 			else:
-				d[cells[0].text] = cleaner(cells[1].text,'\n')
+				d[cells[0].text] = cleaner(cells[1].text, bad)
 	return S_format(d).d_sort(crit)
 
 
