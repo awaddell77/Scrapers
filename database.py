ed0afa863e174cb38df78f39093b1a14e421e07b
awaddell77 <waddell.andrew@gmail.com>
2016-07-18 16:19:02 -0700

    Added functions to create tables and insert rows

--- database.py
+++ database.py
@@ -6,6 +6,41 @@ from soupclass8 import C_sort,r_csv
 def import_data(x):
 	info = C_sort(x)
 	db_columns = info.row(0)
-	
+	db_contents = info.contents[1:]
 
-	return db_columns
+
+	return db_columns, db_contents
+
+def table_construct(x, command):
+	#creates table of text columns usings the provided list as column headers
+	new = [x[i] + " TEXT" for i in range(0, len(x))]
+	contents = ', '.join(new)
+	return command + ' ' + contents + ' ;'
+
+def table_data_prep(x):
+	#adds quotes to every item on a csv list
+	for i in range(0, len(x)):
+		for i_2 in range(0, len(x[i])):
+			x[i][i_2] = '"' + x[i][i_2] + '"'
+	return x 
+def table_insert(x, table):
+	results = []
+
+	for i in range(0, len(x)):
+		values = '(' + ','.join(x[i]) + ') ;'
+		command = 'INSERT INTO %s VALUES %s' % (table, values)
+		print(command)
+		results.append(command)
+
+
+
+
+
+
+
+
+
+
+
+
+
