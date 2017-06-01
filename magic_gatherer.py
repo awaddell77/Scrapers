a72f5a3119b93b9973eb65d35523f21708dadd90
awaddell77 <waddell.andrew@gmail.com>
2017-06-01 12:22:41 -0700

    Scraper for the Gatherer Improvements
    -replaces images with text in card text
    -retrieves card color and cost

--- magic_gatherer.py
+++ magic_gatherer.py
@@ -1,19 +1,48 @@
 #magic 
 #srapes a set page http://gatherer.wizards.com/Pages/Search/Default.aspx?set=[%22Duel%20Decks:%20Mind%20vs.%20Might%22]
 from soupclass8 import *
+import time
 class Magic_scrape:
 	def __init__(self, url):
 		self.url = url
 		self.session = Sel_session(url)
 		self.session.start()
 		self.cards = []
-		self.headers = ["Name", "Card Type", "Pow / Tgh", "Card Text", "Rarity", "Image Link"]
+		self.headers = ["Name", "Card Type", "Pow / Tgh", "Card Text", "Rarity", "Color", "Cost", "Image Link"]
 
 	def page_scrape(self):
-		site = self.session.source()
-		card_table = site.find('table',{'class':'cardItemTable'})
-		card_table_rows = card_table.find_all('tr', {'class':'cardItem evenItem'}) 
-		card_table_rows.extend(card_table.find_all('tr', {'class':'cardItem oddItem'}))
+		card_table_rows = []
+		while(True):
+			site = self.session.source()
+			card_table = site.find('table',{'class':'cardItemTable'})
+			card_table_rows.extend(card_table.find_all('tr', {'class':'cardItem evenItem'}))
+			card_table_rows.extend(card_table.find_all('tr', {'class':'cardItem oddItem'}))
+			command = '''
+			function pageCheck(){
+			var res = true
+			var h =  "ctl00_ctl00_ctl00_MainContent_SubContent_topPagingControlsContainer";
+			var element = document.getElementById(h).children[document.getElementById(h).children.length - 1];
+			if (element.getAttribute("style") == "visibility:hidden;" && !(element.getAttribute("style") === null) ){
+				return false;
+			}
+			return true}
+			return pageCheck()
+			'''
+			test =  self.session.driver.execute_script(command)
+			print(test)
+			if test:
+				print("RETURNED TRUE")
+				command1 = '''
+
+				var buttons = document.getElementById("ctl00_ctl00_ctl00_MainContent_SubContent_topPagingControlsContainer");
+				var element = buttons.children[buttons.children.length - 1]
+				element.click();'''
+				self.session.driver.execute_script(command1)
+				time.sleep(3)
+
+			elif not self.session.driver.execute_script("return {0}".format(command)):
+				break
+
 		#card_info = [i.find('div', {'class':'cardInfo'}) for i in card_table_rows]
 		for i in card_table_rows:
 			self.cards.append(self.__card_info_get(i))
@@ -26,7 +55,14 @@ class Magic_scrape:
 		d = {}
 		card_info = x.find('div', {'class':'cardInfo'})
 		d["Name"] = re.sub('\n', '', card_info.find('span', {'class':'cardTitle'}).text)
+		#print("Processing {0}".format(d["Name"]))
 		type_attack = card_info.find('span', {'class':'typeLine'}).text
+		mana = card_info.find("span",{'class':'manaCost'})
+		cost_color = self.mana_cost(mana)
+		d["Cost"] = cost_color[0]
+		d["Color"] = cost_color[1]
+		#artifact 
+
 		if re.search('\(\d/\d\)', type_attack) != None:
 			t_a = re.search('\(\d/\d\)', type_attack)
 			d["Pow / Tgh"] = re.sub('\n', '', t_a.group())
@@ -37,15 +73,77 @@ class Magic_scrape:
 		d["Card Text"] = self.paragraph_form(card_info.find('div', {'class':'rulesText'})).text.replace('\n', ' ')
 		rarity = S_format(str(x.find('td', {'class':'rightCol setVersions'}).find('div',{'class':'rightCol'}).img)).linkf('<img alt=')
 		d["Rarity"] = self.rarity_find(rarity)
+		if "Artifact" in d["Card Type"]:
+			d["Color"] = "Artifact"
+		elif "Land" in d["Card Type"]:
+			d["Color"] = "Land"
 		image_r = x.find('img')
 		d["Image Link"] = ''
 		if image_r is not None:
 			d["Image Link"] = "http://gatherer.wizards.com/" + S_format(str(image_r)).linkf('src=').replace('../', '')
 		return d
+	def mana_cost(self, x):
+		colors = {'Green':'G', 'Red':'R', "Black":"B", "Blue":"U", "White":"W", "Colorless":"C", "Variable Colorless":"X"}
+		color = ''
+		images = x.find_all('img')
+		cost = ''
+		for i in images:
+			element = S_format(str(i)).linkf('alt=')
+			if element.isdigit():
+				cost += element
+			elif not element.isdigit() and ' or ' in element:
+				sects = element.split(' or ')
+				cost += "(" + colors[sects[0]] + "/" + colors[sects[1]] + ")"
+
+			else:
+				cost += colors[element]
+		for i in cost:
+			#if i isn't a number and color has not been set
+			if i == 'C':
+				color = ''
+			if not i.isdigit() and not color:
+				color = i
+			elif '/' in cost:
+				#need to add something here to check for hybrid mono-lands (See: "Beseech the Queen")
+				color = "Multi-Color"
+				return (cost, color)
+			elif not i.isdigit() and '' not in color:
+				color = "Mult-Color"
+				return (cost, color)
+			elif not i.isdigit() and color and color != i:
+				color = "Multi-Color"
+				return (cost, color)
+			'''else:
+				#remember to fix string after testing
+				color = "ElseMulti-Color"
+				return (cost, color)'''
+		#possibly redundant return statement
+		return (cost, color)
+
+
+
+
+
 	def paragraph_form(self, x):
+		x = self.p_image_process(x)
 		if x.find('p') is not None:
 			for i in x.find_all('p'):
 				i.insert_after(' ')
+
+		return x
+	def p_image_process(self, x):
+		colors = {'Green':'G', 'Red':'R', "Black":"B", "Blue":"U", "White":"W", "Colorless":"C", "Variable Colorless":"X", "Tap":"T"}
+		images = x.find_all("img")
+		if images is None:
+			return x
+		for i in images:
+			#print(i)
+			image = S_format(str(i)).linkf("alt=")
+			#if i isn't a number and color has not been set
+			if image.isdigit():
+				i.replace_with(str(image))
+			if not image.isdigit():
+				i.replace_with(str(colors[image]))
 		return x
 	def m_csv(self):
 		results = []
