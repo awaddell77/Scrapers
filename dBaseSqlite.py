215ecddf7092f644b5f7d7b6c532de6e742ab065
awaddell77 <waddell.andrew@gmail.com>
2017-07-20 15:55:47 -0700

    SQLite object

---
+++ dBaseSqlite.py
@@ -0,0 +1,26 @@
+#sqlite3
+import sqlite3
+
+class dBaseSqlite:
+    def __init__(self, databaseFile):
+        self.databaseFile = databaseFile
+        if not isinstance(self.databaseFile, str): raise TypeError("Must be str")
+        self.con = sqlite3.connect(self.databaseFile)
+        self.cursor = self.con.cursor()
+        self.last_resp = ''
+    def query(self, x):
+        #returns a 2d list object containing tuples
+        #e.g. [(1, 'ITEM #1'), (2, 'ITEM #2'), (3, 'ITEM #3')]
+        self.cursor.execute(x)
+        rows = self.cursor.fetchall()
+        self.last_resp = rows
+        return rows
+
+#for testing only
+test = dBaseSqlite('C:\\Users\\Owner\\testDB.db')
+try:
+    test2 = dBaseSqlite(test)
+except TypeError as TE:
+    print("Raised the TypeError")
+res = test.query('SELECT * from testTable;')
+print(res)
